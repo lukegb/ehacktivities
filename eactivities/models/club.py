1f5126bd2768ef33fed47b90897fe3697f84ac0d
Luke Granger-Brown <git@lukegb.com>
2014-05-08 01:26:32 +0100

    Add key list collapsing to members list

--- eactivities/models/club.py
+++ eactivities/models/club.py
@@ -4,9 +4,25 @@ from .finances import Finances
 from eactivities.parsers.club import ClubParser, MembersListParser
 
 
-def collapse_key_list_data(raw_members_list_data, key_list_data):
-    # TODO: collapse key_list_data into members list
-    return raw_members_list_data
+def collapse_key_list_data(club_id, raw_members_list_data, key_list_data):
+    mld = list(raw_members_list_data)
+    cid_data = {}
+    for key_list in key_list_data.values():
+        for person in key_list.people:
+            cid = person.cid
+            cid_data.setdefault(
+                cid, {'active': {}, 'inactive': {}}
+            )['active' if person.active else 'inactive'].setdefault(
+                unicode(club_id), []
+            ).append(key_list.id)
+
+    for person in mld:
+        if person['cid'] in cid_data:
+            person['key_list_memberships'] = cid_data[person['cid']]
+        else:
+            person['key_list_memberships'] = {'active': {}, 'inactive': {}}
+
+    return mld
 
 
 class ClubMember(Model):
@@ -26,11 +42,12 @@ class ClubMembership(Model):
         'membership_cost', 'associate_members'
     ]
 
-    def list(self, augmented=False):
+    def list(self, with_key_lists=False):
         data = MembersListParser.fetch(self._eactivities, id=self._parent.id)
 
-        if augmented:
+        if with_key_lists:
             data = collapse_key_list_data(
+                self._parent.id,
                 data,
                 self._parent.documentation().key_lists()
             )
